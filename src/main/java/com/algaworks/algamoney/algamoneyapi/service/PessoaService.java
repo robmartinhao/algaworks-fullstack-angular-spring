d48d57ec6947e339447f43b7598fd69ff12aaddb
Robson <robmartinhao@gmail.com>
2020-10-06 08:26:30 -0300

    4.4. Implementando atualização parcial com PUT

--- src/main/java/com/algaworks/algamoney/algamoneyapi/service/PessoaService.java
+++ src/main/java/com/algaworks/algamoney/algamoneyapi/service/PessoaService.java
@@ -14,9 +14,20 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     public Pessoa atualizar(Long codigo, Pessoa pessoa) {
-        Pessoa pessoaSalva = pessoaRepository.findById(codigo)
-                .orElseThrow(() -> new EmptyResultDataAccessException(1));
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
         BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
         return  pessoaRepository.save(pessoaSalva);
+    }
+
+    public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+        pessoaSalva.setAtivo(ativo);
+        pessoaRepository.save(pessoaSalva);
+    }
+
+    private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+        Pessoa pessoaSalva = pessoaRepository.findById(codigo)
+                .orElseThrow(() -> new EmptyResultDataAccessException(1));
+        return pessoaSalva;
     }
 }
