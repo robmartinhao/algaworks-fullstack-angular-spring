d6de6f9edf6e96c600719e64ae3e01f0b45ba9ba
Robson <robmartinhao@gmail.com>
2020-10-28 19:49:30 -0300

    6.13. Desafio: Finalizando permissões de acesso

--- src/main/java/com/algaworks/algamoney/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/algamoneyapi/resource/LancamentoResource.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
@@ -40,11 +41,13 @@ public class LancamentoResource {
     private MessageSource messageSource;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
     public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
     public ResponseEntity<Lancamento> buscarPeloCodigo (@PathVariable Long codigo) {
         return lancamentoRepository.findById(codigo)
                 .map(lancamento -> ResponseEntity.ok(lancamento))
@@ -52,6 +55,7 @@ public class LancamentoResource {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
     public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 
         Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
@@ -69,6 +73,7 @@ public class LancamentoResource {
     }
 
     @DeleteMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and #oauth2.hasScope('write')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long codigo) {
         lancamentoRepository.deleteById(codigo);
