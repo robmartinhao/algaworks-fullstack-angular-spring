83f467525b28b7606088de04ee4adcdd3983d275
Robson <robmartinhao@gmail.com>
2020-11-04 21:39:37 -0300

    Ajuste migration

--- src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -12,8 +12,8 @@ CREATE TABLE pessoa (
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Roberto', true, "Rua São Paulo", "10", "comp", "Jd. Alfazemas", "03138-000", "São Paulo", "SP");
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('José', false, "Rua Minas Gerais", "10", "comp1", "Alpina", "02521-000", "São Paulo", "SP");
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Luiz', true, "Rua Porto", "10", "comp2", "Jd. Clímaco", "78999-123", "São Paulo", "SP");
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Dário', false, "Rua Agora", "10", "comp3", "Jd. Rondon", "03500-100", "São Paulo", "SP");
-INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Wesley', true, "Rua Firula", "10", "comp4", "Vila Nove de Julho", "13238-020", "São Paulo", "SP");
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Roberto', true, 'Rua São Paulo', '10', 'comp', 'Jd. Alfazemas', '03138-000', 'São Paulo', 'SP');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('José', false, 'Rua Minas Gerais', '10', 'comp1', 'Alpina', '02521-000', 'São Paulo', 'SP');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Luiz', true, 'Rua Porto', '10', 'comp2', 'Jd. Clímaco', '78999-123', 'São Paulo', 'SP');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Dário', false, 'Rua Agora', '10', 'comp3', 'Jd. Rondon', '03500-100', 'São Paulo', 'SP');
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values ('Wesley', true, 'Rua Firula', '10', 'comp4', 'Vila Nove de Julho', '13238-020', 'São Paulo', 'SP');
