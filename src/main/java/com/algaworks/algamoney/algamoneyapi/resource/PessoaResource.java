cce63f61a65c7a0afd572bad021b490910d7b17c
Robson <robmartinhao@gmail.com>
2020-11-06 21:43:04 -0300

    7.7. Desafio: Pesquisa de pessoa

--- src/main/java/com/algaworks/algamoney/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/algamoneyapi/resource/PessoaResource.java
@@ -6,6 +6,8 @@ import com.algaworks.algamoney.algamoneyapi.repository.PessoaRepository;
 import com.algaworks.algamoney.algamoneyapi.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -30,8 +33,8 @@ public class PessoaResource {
 
     @GetMapping
     @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-    public List<Pessoa> listar() {
-        return pessoaRepository.findAll();
+    public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable) {
+        return pessoaRepository.findByNomeContaining(nome, pageable);
     }
 
     @PostMapping
