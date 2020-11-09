da2d0011d062de36318aa28017b813c733e5e6e3
Robson <robmartinhao@gmail.com>
2020-11-09 20:21:06 -0300

    7.9. Desafio: Atualização de lançamento

--- src/main/java/com/algaworks/algamoney/algamoneyapi/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/algamoneyapi/service/LancamentoService.java
@@ -1,11 +1,15 @@
 package com.algaworks.algamoney.algamoneyapi.service;
 
 import com.algaworks.algamoney.algamoneyapi.model.Lancamento;
+import com.algaworks.algamoney.algamoneyapi.model.Pessoa;
 import com.algaworks.algamoney.algamoneyapi.repository.LancamentoRepository;
 import com.algaworks.algamoney.algamoneyapi.repository.PessoaRepository;
 import com.algaworks.algamoney.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LancamentoService {
@@ -24,5 +28,22 @@ public class LancamentoService {
                 .orElseThrow(() -> new PessoaInexistenteOuInativaException());
 
         return lancamentoRepository.save(lancamento);
+    }
+
+    public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+        Lancamento lancamentoSalvo = buscarLancamentoExitente(codigo);
+
+        Optional.ofNullable(lancamento).map(Lancamento::getPessoa)
+                .filter(
+                    pessoa -> pessoa.getCodigo().equals(lancamentoSalvo.getPessoa().getCodigo()) && !lancamentoSalvo.getPessoa().isInativo())
+                .orElseThrow(PessoaInexistenteOuInativaException::new);
+
+        BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+        return lancamentoRepository.save(lancamentoSalvo);
+    }
+
+    private Lancamento buscarLancamentoExitente(Long codigo) {
+        return lancamentoRepository.findById(codigo).orElseThrow(IllegalArgumentException::new);
     }
 }
